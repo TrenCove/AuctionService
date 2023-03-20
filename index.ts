8f5a2c25a4663f8e1c4bee213eba8d85719cf3cf
dokdek <dokdek@gmail.com>
2023-03-19 22:04:52 -0400

    auction check wip

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import {authenticateToken} from './middleware/authenticateToken';
 import * as cron from 'node-cron';
+import { CheckAuction } from "./CheckAuction";
 
 const app: Express = express();
 app.use(bodyParser.json());
@@ -10,7 +11,10 @@ app.use(
     extended: true,
   })
 );
-const port = 3000;
+const port = 3001;
+
+const pubSubList = new Map<string, string[]>(); //mapping itemID to usernames
+const userSockets = new Map<string, string>(); //mapping username to socket to send notif out
 
 
 app.post('/publish', authenticateToken, (req: Request, res: Response)=>{
@@ -25,7 +29,29 @@ app.post('/unsubscribe', authenticateToken, (req: Request, res: Response)=>{
     //todo
 });
 
-cron.schedule('* * * * *', () => {
-    //todo
+cron.schedule('* * * * *', async () => {
     console.log("Running auction check");
-})
+    const dueItems = await CheckAuction();
+    if(dueItems.length > 0){
+        dueItems.forEach((item)=>{
+            publish(item);
+        })
+    }
+});
+
+function publish(itemID: string){
+    const users = pubSubList.get(itemID);
+    if(users && users.length > 0){
+        //TODO: send the users connected via websocket
+    }
+};
+
+function subscribe(username: string, itemID: string){
+    const currentWatchingUsers = pubSubList.get(itemID);
+    if(currentWatchingUsers != undefined){
+        currentWatchingUsers.push(username);
+        pubSubList.set(itemID, currentWatchingUsers);
+    }else{
+        pubSubList.set(itemID, [username]);
+    }
+}
